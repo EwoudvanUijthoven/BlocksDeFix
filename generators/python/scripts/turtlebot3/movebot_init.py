85260f9dd6632b456b188d63cd216911784eea05
Ewoud <e.k.v.uijthoven@student.tue.nl>
2024-09-12 22:32:30 +0200

    feat(MSC-27): movebot_sec python code

---
+++ generators/python/scripts/turtlebot3/movebot_init.py
@@ -0,0 +1,13 @@
+import rospy
+from geometry_msgs.msg import Twist
+import time
+
+rospy.init_node('movebot_node', anonymous=True)
+
+velocity_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+rate = rospy.Rate(10)
+twist = Twist()
+start = time.time()
+flag = True
+twist.linear.z = 0.00
