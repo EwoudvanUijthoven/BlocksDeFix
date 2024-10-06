6320dee362aa01ab08eaaa60aa12322702a3bdc1
Ewoud van Uijthoven <e.k.v.uijthoven@student.tue.nl>
2024-10-06 20:49:12 +0200

    feat(MSC-40): Implement stop_robot block

---
+++ generators/python/scripts/turtlebot3/blocks/stop_bot.py
@@ -0,0 +1,27 @@
+def stop_robot():
+    rate = rospy.Rate(10)
+    twist = geometry_msgs.Twist()
+    start = time.time()
+    flag = True
+
+    twist.linear.x = 0.00
+    twist.linear.y = 0.00
+    twist.linear.z = 0.00
+    twist.angular.x = 0.00
+    twist.angular.y = 0.00
+    twist.angular.z = 0.00
+
+    while not rospy.is_shutdown() and flag:
+        sample_time = time.time()
+        if ((sample_time - start) > 2):
+            flag = False
+            break
+        velocity_pub.publish(twist)
+    twist = geometry_msgs.Twist()
+    velocity_pub.publish(twist)
+    rate.sleep()
+
+    rospy.loginfo("*********************ROBOT STOPED**************************")
+
+    return 'Shutdown Set!'
+
