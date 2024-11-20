e565856a069a0c09c43c0b27892225baf9f372ce
Ewoud <e.k.v.uijthoven@student.tue.nl>
2024-11-20 17:38:20 +0100

    feat(MSC-64): implemented timer and logged elapsed time and amount of run clicks

--- main/saveTime.php
+++
@@ -1,6 +0,0 @@
-<?php
-$code = file_get_contents("php://input");
-if (strlen($code)> 1) {
-    fwrite($handle = fopen("logs/time_Task_".$_REQUEST["task"]."_area_".$_REQUEST["area"]."_".time().".log", 'w'), $code);
-}
-?>
